49f6d89881182fe570ee0419bd402120c772db22
chekst <israel.gavriel@gmail.com>
2018-05-06 11:53:29 +0800

    remove comments

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,10 +2,6 @@ defmodule PhoenixApp.PageController do
   use PhoenixApp.Web, :controller
 
   def index(conn, _params) do
-    #Chat.Registry.create(Chat.Registry, "user_id")
-    #{:ok, user} = Chat.Registry.lookup(Chat.Registry, "user_id")
-    #Chat.User.put(user, "id", 1)
-    #IO.puts Chat.User.get(user, "id")
     render conn, "index.html"
   end
 end
