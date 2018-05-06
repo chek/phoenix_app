541d00e81f2f07fb7720f362e5757f64043ccf24
chekst <israel.gavriel@gmail.com>
2018-05-06 11:30:18 +0800

    refactor channel

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -9,10 +9,7 @@ defmodule PhoenixApp.RoomChannel do
     end
 
     def handle_in("connect", %{"uuid" => uuid}, socket) do
-        IO.puts 'handle connect msg'
         socket = assign(socket, :uuid, uuid)
-        #IO.puts 'new uuid'
-        #IO.puts socket.assigns.uuid
         Chat.Registry.create(Chat.Registry, uuid)
         params = %{uuid: uuid}
         broadcast! socket, "connect", %{params: params}
@@ -20,38 +17,16 @@ defmodule PhoenixApp.RoomChannel do
     end    
 
     def handle_in("message", %{"message" => message}, socket) do
-        IO.puts 'handle message msg'
-        #socket = assign(socket, :uuid, uuid)
-        #IO.puts 'new uuid'
-        #IO.puts socket.assigns.uuid
-        #Chat.Registry.create(Chat.Registry, uuid)
         {:ok, user} = Chat.Registry.lookup(Chat.Registry, socket.assigns.uuid)
-        location = Chat.User.get(user, "location")
-        
+        location = Chat.User.get(user, "location")        
         params = %{uuid: socket.assigns.uuid, location: location, message: message}
         broadcast! socket, "message", %{params: params}
         {:noreply, socket}
     end    
 
     def handle_in("location", %{"location" => location}, socket) do
-        IO.puts 'handle location msg'
-        #socket = assign(socket, :uuid, uuid)
-        IO.puts socket.assigns.uuid
-        IO.inspect location
-        #IO.puts location
-        #IO.puts socket.assigns.uuid
-        #Chat.Registry.create(Chat.Registry, uuid)
-        #{:ok, users} = 
-        #IO.inspect users
-        #for  {uuid, user}  <-  users  do
-        #    loc = Chat.User.get(user, "location")
-        #    IO.puts "#{uuid} --> #{loc}"
-        #end
-        
         {:ok, user} = Chat.Registry.lookup(Chat.Registry, socket.assigns.uuid)
         Chat.User.put(user, "location", location)
-        #loc = Chat.User.get(user, "location")
-        #IO.inspect loc
         params = %{uuid: socket.assigns.uuid, location: location}
         broadcast! socket, "location", %{params: params}
         {:noreply, socket}
